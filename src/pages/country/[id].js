fee6bf2abfe2867d50540aeacc185eda7b07ade3
conganh649 <tranconganh1999@gmail.com>
2021-08-23 13:57:07 +0700

    Update css

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -24,80 +24,89 @@ const Country = ({ country }) => {
 
   return (
     <Layout title={country.name}>
-      <div>
-        <div className={styles.overview_panel}>
-          <img src={country.flag} alt={country.name}></img>
-          <h1 className={styles.overview_name}>{country.name}</h1>
-          <div className={styles.overview_region}>{country.region}</div>
+      <div className={styles.container}>
+        <div className={styles.container_left}>
+          <div className={styles.overview_panel}>
+            <img src={country.flag} alt={country.name}></img>
+            <h1 className={styles.overview_name}>{country.name}</h1>
+            <div className={styles.overview_region}>{country.region}</div>
 
-          <div className={styles.overview_numbers}>
-            <div className={styles.overview_population}>
-              <div className={styles.overview_value}>{country.population}</div>
-              <div className={styles.overview_label}>Population</div>
-            </div>
-            <div className={styles.overview_area}>
-              <div className={styles.overview_value}>
-                {country.area || "N/A"}
+            <div className={styles.overview_numbers}>
+              <div className={styles.overview_population}>
+                <div className={styles.overview_value}>
+                  {country.population}
+                </div>
+                <div className={styles.overview_label}>Population</div>
               </div>
-              <div className={styles.overview_label}>Area</div>
+              <div className={styles.overview_area}>
+                <div className={styles.overview_value}>
+                  {country.area || "N/A"}
+                </div>
+                <div className={styles.overview_label}>Area</div>
+              </div>
             </div>
           </div>
         </div>
-        <div className={styles.details_panel}>
-          <h4 className={styles.details_panel_heading}>Details</h4>
-          <div className={styles.details_panel_row}>
-            <div className={styles.details_panel_label}>Capital</div>
-            <div className={styles.details_panel_value}>{country.capital}</div>
-          </div>
 
-          <div className={styles.details_panel_row}>
-            <div className={styles.details_panel_label}>Languages</div>
-            <div className={styles.details_panel_value}>
-              {country.languages.map(({ name }) => name).join(", ")}
+        <div className={styles.container_right}>
+          <div className={styles.details_panel}>
+            <h4 className={styles.details_panel_heading}>Details</h4>
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Capital</div>
+              <div className={styles.details_panel_value}>
+                {country.capital}
+              </div>
             </div>
-          </div>
 
-          <div className={styles.details_panel_row}>
-            <div className={styles.details_panel_label}>Currencies</div>
-            <div className={styles.details_panel_value}>
-              {country.currencies.map(({ name }) => name).join(", ")}
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Languages</div>
+              <div className={styles.details_panel_value}>
+                {country.languages.map(({ name }) => name).join(", ")}
+              </div>
             </div>
-          </div>
 
-          <div className={styles.details_panel_row}>
-            <div className={styles.details_panel_label}>Native name</div>
-            <div className={styles.details_panel_value}>
-              {country.nativeName}
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Currencies</div>
+              <div className={styles.details_panel_value}>
+                {country.currencies.map(({ name }) => name).join(", ")}
+              </div>
             </div>
-          </div>
 
-          <div className={styles.details_panel_row}>
-            <div className={styles.details_panel_label}>Subregion</div>
-            <div className={styles.details_panel_value}>
-              {country.subregion}
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Native name</div>
+              <div className={styles.details_panel_value}>
+                {country.nativeName}
+              </div>
             </div>
-          </div>
 
-          <div className={styles.details_panel_row}>
-            <div className={styles.details_panel_label}>Gini</div>
-            <div className={styles.details_panel_value}>
-              {country.gini || "N/A"} %
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Subregion</div>
+              <div className={styles.details_panel_value}>
+                {country.subregion}
+              </div>
             </div>
-          </div>
 
-          <div className={styles.details_panel_borders}>
-            <div className={styles.details_panel_borders_label}>
-              Neighboring Countries
+            <div className={styles.details_panel_row}>
+              <div className={styles.details_panel_label}>Gini</div>
+              <div className={styles.details_panel_value}>
+                {country.gini || "N/A"} %
+              </div>
             </div>
-            <div className={styles.details_panel_borders_container}>
-              {borders.map(({ flag, name }) => (
-                <div className={styles.details_panel_borders_country}>
-                  <img src={flag} alt={name}></img>
-                  <div className={styles.details_panel_borders_name}>
-                    {name}
+
+            <div className={styles.details_panel_borders}>
+              <div className={styles.details_panel_borders_label}>
+                Neighboring Countries
+              </div>
+              <div className={styles.details_panel_borders_container}>
+                {borders.map(({ flag, name }) => (
+                  <div className={styles.details_panel_borders_country}>
+                    <img src={flag} alt={name}></img>
+                    <div className={styles.details_panel_borders_name}>
+                      {name}
+                    </div>
                   </div>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
         </div>
